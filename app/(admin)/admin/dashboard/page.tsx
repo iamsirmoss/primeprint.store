d96ee5a69d459974460a4b3a64ab0a4a3d7a7141
iamsirmoss <48318707+iamsirmoss@users.noreply.github.com>
2026-03-13 22:52:01 +0000

    admin begin

--- app/(admin)/admin/dashboard/page.tsx
+++ app/(admin)/admin/dashboard/page.tsx
@@ -1,16 +1,119 @@
-import { auth } from "@/lib/auth"
-import { redirect } from "next/navigation"
-import { headers } from "next/headers"
-import { DeleteUserButton, PlaceholderDeleteUserButton } from "@/components/delete-user-button";
-import UserRoleSelect from "@/components/user-role-select";
-import { UserRole } from "@/lib/generated/prisma/enums";
+// import { auth } from "@/lib/auth"
+// import { redirect } from "next/navigation"
+// import { headers } from "next/headers"
+// import { DeleteUserButton, PlaceholderDeleteUserButton } from "@/components/delete-user-button";
+// import UserRoleSelect from "@/components/user-role-select";
+// import { UserRole } from "@/lib/generated/prisma/enums";
+// import ReturnButton from "@/components/return-button";
+// import GradientBox from "@/components/Admin/dashboards/Dashboard2/GradientBox";
+// import Subscriptions from "@/components/Admin/dashboards/Dashboard2/Subscriptions";
+// import UsersBox from "@/components/Admin/dashboards/Dashboard2/UsersBox";
+// import RevenueForcastChart from "@/components/Admin/dashboards/Dashboard2/RevenueForcastChart";
+// import AnnualProfit from "@/components/Admin/dashboards/Dashboard2/AnnualProfit";
+// import type { Metadata } from "next";
+// import NewCustomers from "@/components/Admin/dashboards/Dashboard2/NewCustomers";
+// import TotalIncome from "@/components/Admin/dashboards/Dashboard2/TotalIncome";
+// import WeeklySchedule from "@/components/Admin/dashboards/Dashboard2/WeeklySchedule";
+// import RevenueByProduct from "@/components/Admin/dashboards/Dashboard2/RevenueByProduct";
+// import SalesFromLocation from "@/components/Admin/dashboards/Dashboard2/SalesFromLocation";
+// import WeeklyStats from "@/components/Admin/dashboards/Dashboard2/WeeklyStats";
+// import DailyActivities from "@/components/Admin/dashboards/Dashboard2/DailyActivivities";
+// import FigmaCard from "@/components/Admin/dashboards/Dashboard2/FigmaCard";
+
+// const page = async () => {
+//    const headerList = await headers();
+
+//       const session = await auth.api.getSession({
+//             headers: headerList,
+//       })
+
+//       if (!session) redirect('/login');
+
+//       if (session.user.role !== "ADMIN") {
+
+//             return (
+//                   <div className='px-4 xl:px-14 xxl:px-40 xll:px-80 xxx:px-[22%] lll:px-[25%] mt-16 mb-40 min-h-screen'>
+//                         <ReturnButton href='/' label='Back to Home' />
+//                         <hr />
+//                               <div className="py-10 w-[90%] sm:w-[80%] md:w-[70%] lg:w-[50%] xl:w-[45%] mx-auto flex flex-col justify-center items-center">
+//                                     <div className='mb-8 bg-slate-200 py-3 w-full'>
+//                                           <h5 className='text-center font-semibold text-2xl'>Admin Dashboard</h5>
+//                                     </div>
+//                                     <p className="p-2 rounded-md text-lg bg-red-700 text-white font-bold">FORBIDDEN</p>
+//                               </div>
+//                   </div>
+//                   )}
+
+//             const { users } = await auth.api.listUsers({
+//                   headers: headerList,
+//                   query: { 
+//                         sortBy: "name",
+//                    }
+//             })
+
+//             const sortedUsers = users.sort((a, b) => {
+//                   if (a.role === "ADMIN" && b.role !== "ADMIN") return -1;
+//                   if (a.role !== "ADMIN" && b.role === "ADMIN") return 1;
+//                   return 0
+//             })
+            
+//             return (
+//                   <>
+//                         <div className="grid grid-cols-12 gap-7">
+//                               <div className="lg:col-span-6 col-span-12">
+//                                     <GradientBox user={session.user} />
+//                               </div>
+//                               <div className="lg:col-span-3 md:col-span-6 col-span-12">
+//                                     <Subscriptions />
+//                               </div>
+//                               <div className="lg:col-span-3 md:col-span-6 col-span-12">
+//                                     <UsersBox />
+//                               </div>
+//                               <div className="lg:col-span-8 col-span-12">
+//                                     <RevenueForcastChart />
+//                               </div>
+//                               <div className="lg:col-span-4 col-span-12">
+//                                     <AnnualProfit />
+//                               </div>
+//                               <div className="lg:col-span-4 col-span-12">
+//                                     <NewCustomers />
+//                                     <TotalIncome />
+//                               </div>
+//                               <div className="lg:col-span-8 col-span-12">
+//                                     <WeeklySchedule />
+//                               </div>
+//                               <div className="lg:col-span-8 col-span-12">
+//                                     <RevenueByProduct />
+//                               </div>
+//                               <div className="lg:col-span-4 md:col-span-6 col-span-12">
+//                                     <SalesFromLocation />
+//                               </div>
+//                               <div className="lg:col-span-4 md:col-span-6 col-span-12">
+//                                     <WeeklyStats />
+//                               </div>
+//                               <div className="lg:col-span-4 md:col-span-6 col-span-12">
+//                                     <DailyActivities />
+//                               </div>
+//                               <div className="lg:col-span-4 md:col-span-6 col-span-12">
+//                                     <FigmaCard />
+//                               </div>
+//                         </div>
+//                   </>
+//             )
+
+//       }
+
+// export default page
+
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
 import ReturnButton from "@/components/return-button";
 import GradientBox from "@/components/Admin/dashboards/Dashboard2/GradientBox";
 import Subscriptions from "@/components/Admin/dashboards/Dashboard2/Subscriptions";
 import UsersBox from "@/components/Admin/dashboards/Dashboard2/UsersBox";
 import RevenueForcastChart from "@/components/Admin/dashboards/Dashboard2/RevenueForcastChart";
 import AnnualProfit from "@/components/Admin/dashboards/Dashboard2/AnnualProfit";
-import type { Metadata } from "next";
 import NewCustomers from "@/components/Admin/dashboards/Dashboard2/NewCustomers";
 import TotalIncome from "@/components/Admin/dashboards/Dashboard2/TotalIncome";
 import WeeklySchedule from "@/components/Admin/dashboards/Dashboard2/WeeklySchedule";
@@ -20,87 +123,75 @@ import WeeklyStats from "@/components/Admin/dashboards/Dashboard2/WeeklyStats";
 import DailyActivities from "@/components/Admin/dashboards/Dashboard2/DailyActivivities";
 import FigmaCard from "@/components/Admin/dashboards/Dashboard2/FigmaCard";
 
-const page = async () => {
-   const headerList = await headers();
+const Page = async () => {
+  const headerList = await headers();
 
-      const session = await auth.api.getSession({
-            headers: headerList,
-      })
+  const session = await auth.api.getSession({
+    headers: headerList,
+  });
 
-      if (!session) redirect('/login');
+  if (!session) redirect("/login");
 
-      if (session.user.role !== "ADMIN") {
+  if (session.user.role !== "ADMIN") {
+    return (
+      <div className="px-4 xl:px-14 xxl:px-40 xll:px-80 xxx:px-[22%] lll:px-[25%] mt-16 mb-40 min-h-screen">
+        <ReturnButton href="/" label="Back to Home" />
+        <hr />
+        <div className="py-10 w-[90%] sm:w-[80%] md:w-[70%] lg:w-[50%] xl:w-[45%] mx-auto flex flex-col justify-center items-center">
+          <div className="mb-8 bg-slate-200 py-3 w-full">
+            <h5 className="text-center font-semibold text-2xl">
+              Admin Dashboard
+            </h5>
+          </div>
+          <p className="p-2 rounded-md text-lg bg-red-700 text-white font-bold">
+            FORBIDDEN
+          </p>
+        </div>
+      </div>
+    );
+  }
 
-            return (
-                  <div className='px-4 xl:px-14 xxl:px-40 xll:px-80 xxx:px-[22%] lll:px-[25%] mt-16 mb-40 min-h-screen'>
-                        <ReturnButton href='/' label='Back to Home' />
-                        <hr />
-                              <div className="py-10 w-[90%] sm:w-[80%] md:w-[70%] lg:w-[50%] xl:w-[45%] mx-auto flex flex-col justify-center items-center">
-                                    <div className='mb-8 bg-slate-200 py-3 w-full'>
-                                          <h5 className='text-center font-semibold text-2xl'>Admin Dashboard</h5>
-                                    </div>
-                                    <p className="p-2 rounded-md text-lg bg-red-700 text-white font-bold">FORBIDDEN</p>
-                              </div>
-                  </div>
-                  )}
+  return (
+    <div className="grid grid-cols-12 gap-7">
+      <div className="lg:col-span-6 col-span-12">
+        <GradientBox user={session.user} />
+      </div>
+      <div className="lg:col-span-3 md:col-span-6 col-span-12">
+        <Subscriptions />
+      </div>
+      <div className="lg:col-span-3 md:col-span-6 col-span-12">
+        <UsersBox />
+      </div>
+      <div className="lg:col-span-8 col-span-12">
+        <RevenueForcastChart />
+      </div>
+      <div className="lg:col-span-4 col-span-12">
+        <AnnualProfit />
+      </div>
+      <div className="lg:col-span-4 col-span-12">
+        <NewCustomers />
+        <TotalIncome />
+      </div>
+      <div className="lg:col-span-8 col-span-12">
+        <WeeklySchedule />
+      </div>
+      <div className="lg:col-span-8 col-span-12">
+        <RevenueByProduct />
+      </div>
+      <div className="lg:col-span-4 md:col-span-6 col-span-12">
+        <SalesFromLocation />
+      </div>
+      <div className="lg:col-span-4 md:col-span-6 col-span-12">
+        <WeeklyStats />
+      </div>
+      <div className="lg:col-span-4 md:col-span-6 col-span-12">
+        <DailyActivities />
+      </div>
+      <div className="lg:col-span-4 md:col-span-6 col-span-12">
+        <FigmaCard />
+      </div>
+    </div>
+  );
+};
 
-            const { users } = await auth.api.listUsers({
-                  headers: headerList,
-                  query: { 
-                        sortBy: "name",
-                   }
-            })
-
-            const sortedUsers = users.sort((a, b) => {
-                  if (a.role === "ADMIN" && b.role !== "ADMIN") return -1;
-                  if (a.role !== "ADMIN" && b.role === "ADMIN") return 1;
-                  return 0
-            })
-            
-            return (
-                  <>
-                        <div className="grid grid-cols-12 gap-7">
-                              <div className="lg:col-span-6 col-span-12">
-                                    <GradientBox user={session.user} />
-                              </div>
-                              <div className="lg:col-span-3 md:col-span-6 col-span-12">
-                                    <Subscriptions />
-                              </div>
-                              <div className="lg:col-span-3 md:col-span-6 col-span-12">
-                                    <UsersBox />
-                              </div>
-                              <div className="lg:col-span-8 col-span-12">
-                                    <RevenueForcastChart />
-                              </div>
-                              <div className="lg:col-span-4 col-span-12">
-                                    <AnnualProfit />
-                              </div>
-                              <div className="lg:col-span-4 col-span-12">
-                                    <NewCustomers />
-                                    <TotalIncome />
-                              </div>
-                              <div className="lg:col-span-8 col-span-12">
-                                    <WeeklySchedule />
-                              </div>
-                              <div className="lg:col-span-8 col-span-12">
-                                    <RevenueByProduct />
-                              </div>
-                              <div className="lg:col-span-4 md:col-span-6 col-span-12">
-                                    <SalesFromLocation />
-                              </div>
-                              <div className="lg:col-span-4 md:col-span-6 col-span-12">
-                                    <WeeklyStats />
-                              </div>
-                              <div className="lg:col-span-4 md:col-span-6 col-span-12">
-                                    <DailyActivities />
-                              </div>
-                              <div className="lg:col-span-4 md:col-span-6 col-span-12">
-                                    <FigmaCard />
-                              </div>
-                        </div>
-                  </>
-            )
-
-      }
-
-export default page
+export default Page;
--- app/(admin)/admin/layout/vertical/header/Profile.tsx
+++ app/(admin)/admin/layout/vertical/header/Profile.tsx
@@ -1,11 +1,113 @@
+// "use client";
+
+// import { Icon } from "@iconify/react";
+// import React from "react";
+// import * as profileData from "./Data";
+// import Link from "next/link";
+// import Image from "next/image";
+// import SimpleBar from "simplebar-react";
+// import {
+//   DropdownMenu,
+//   DropdownMenuTrigger,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+// } from "@/components/ui/dropdown-menu";
+// import { Badge } from "@/components/ui/badge";
+// import { useSession } from "@/lib/auth-client";
+
+// const Profile = () => {
+
+//   const { data: session, isPending } = useSession();
+
+//   return (
+//     <div className="relative">
+//       <DropdownMenu>
+//         {/* === Trigger === */}
+//         <DropdownMenuTrigger asChild>
+//           <div className="flex items-center gap-1 cursor-pointer">
+//             {/* <span className="h-10 w-10 hover:text-primary rounded-full flex justify-center items-center group-hover/menu:bg-lightprimary group-hover/menu:text-primary">
+//               <Image
+//                 src="/images/profile/user-1.jpg"
+//                 alt="User"
+//                 height={35}
+//                 width={35}
+//                 className="rounded-full"
+//               />
+//             </span> */}
+//             <div className="flex items-center justify-center aspect-square size-10 rounded-full bg-gray-400 text-white">
+//               <span className="uppercase text-xs md:text-base font-semibold font-sans">
+//                 {session?.user.name?.slice(0, 1)}
+//               </span>
+//             </div>
+//             <Icon
+//               icon="solar:alt-arrow-down-bold"
+//               className="hover:text-primary dark:text-primary group-hover/menu:text-primary"
+//               height={12}
+//             />
+//           </div>
+//         </DropdownMenuTrigger>
+
+//         {/* === Dropdown Content === */}
+//         <DropdownMenuContent
+//           align="end"
+//           sideOffset={10}
+//           className="w-screen sm:w-[360px] pb-4 rounded-sm border-none bg-white dark:bg-dark shadow-md dark:shadow-dark-md"
+//         >
+//           {/* === Header Section === */}
+//           <div className="px-6">
+//             <div className="flex items-center gap-6 pb-5 border-b border-border dark:border-darkborder mt-5 mb-3">
+//               <div className="flex items-center justify-center aspect-square size-14 rounded-full bg-gray-400 text-white">
+//                 <span className="uppercase text-xs md:text-base font-semibold font-sans">
+//                   {session?.user.name?.slice(0, 1)}
+//                 </span>
+//               </div>
+//               <div>
+//                 <h5 className="text-base font-semibold">
+//                   {session?.user.name} 
+//                 </h5>
+//                 <p className="text-sm text-ld font-medium opacity-60">{session?.user.email}</p>
+//               </div>
+//             </div>
+//           </div>
+
+//           {/* === Menu Items === */}
+//           <SimpleBar>
+//             {profileData.profileDD.map((items, index) => (
+//               <div key={index} className="px-6 mb-2">
+//                 <DropdownMenuItem
+//                   asChild
+//                   className="px-3 py-2 flex justify-between items-center bg-hover group/link w-full rounded-md cursor-pointer focus:bg-hover focus:text-primary"
+//                 >
+//                   <Link href={items.url} className="flex items-center w-full">
+//                     <div className="flex gap-3 w-full">
+//                       <h5 className="text-sm font-normal group-hover/link:text-primary">
+//                         {items.title}
+//                       </h5>
+//                       {items.url === "/apps/invoice" && (
+//                         <Badge variant={"outline"}>
+//                           4
+//                         </Badge>
+//                       )}
+//                     </div>
+//                   </Link>
+//                 </DropdownMenuItem>
+//               </div>
+//             ))}
+//           </SimpleBar>
+//         </DropdownMenuContent>
+//       </DropdownMenu>
+//     </div>
+//   );
+// };
+
+// export default Profile;
+
 "use client";
 
 import { Icon } from "@iconify/react";
 import React from "react";
 import * as profileData from "./Data";
 import Link from "next/link";
-import Image from "next/image";
-import SimpleBar from "simplebar-react";
 import {
   DropdownMenu,
   DropdownMenuTrigger,
@@ -16,84 +118,72 @@ import { Badge } from "@/components/ui/badge";
 import { useSession } from "@/lib/auth-client";
 
 const Profile = () => {
+  const { data: session } = useSession();
 
-  const { data: session, isPending } = useSession();
+  const items = Array.isArray(profileData.profileDD) ? profileData.profileDD : [];
 
   return (
     <div className="relative">
       <DropdownMenu>
-        {/* === Trigger === */}
         <DropdownMenuTrigger asChild>
-          <div className="flex items-center gap-1 cursor-pointer">
-            {/* <span className="h-10 w-10 hover:text-primary rounded-full flex justify-center items-center group-hover/menu:bg-lightprimary group-hover/menu:text-primary">
-              <Image
-                src="/images/profile/user-1.jpg"
-                alt="User"
-                height={35}
-                width={35}
-                className="rounded-full"
-              />
-            </span> */}
+          <button type="button" className="flex items-center gap-1 cursor-pointer">
             <div className="flex items-center justify-center aspect-square size-10 rounded-full bg-gray-400 text-white">
               <span className="uppercase text-xs md:text-base font-semibold font-sans">
-                {session?.user.name?.slice(0, 1)}
+                {session?.user?.name?.slice(0, 1) ?? "U"}
               </span>
             </div>
             <Icon
               icon="solar:alt-arrow-down-bold"
-              className="hover:text-primary dark:text-primary group-hover/menu:text-primary"
+              className="hover:text-primary dark:text-primary"
               height={12}
             />
-          </div>
+          </button>
         </DropdownMenuTrigger>
 
-        {/* === Dropdown Content === */}
         <DropdownMenuContent
           align="end"
           sideOffset={10}
           className="w-screen sm:w-[360px] pb-4 rounded-sm border-none bg-white dark:bg-dark shadow-md dark:shadow-dark-md"
         >
-          {/* === Header Section === */}
           <div className="px-6">
             <div className="flex items-center gap-6 pb-5 border-b border-border dark:border-darkborder mt-5 mb-3">
               <div className="flex items-center justify-center aspect-square size-14 rounded-full bg-gray-400 text-white">
                 <span className="uppercase text-xs md:text-base font-semibold font-sans">
-                  {session?.user.name?.slice(0, 1)}
+                  {session?.user?.name?.slice(0, 1) ?? "U"}
                 </span>
               </div>
               <div>
                 <h5 className="text-base font-semibold">
-                  {session?.user.name} 
+                  {session?.user?.name ?? "User"}
                 </h5>
-                <p className="text-sm text-ld font-medium opacity-60">{session?.user.email}</p>
+                <p className="text-sm text-ld font-medium opacity-60">
+                  {session?.user?.email ?? ""}
+                </p>
               </div>
             </div>
           </div>
 
-          {/* === Menu Items === */}
-          <SimpleBar>
-            {profileData.profileDD.map((items, index) => (
+          <div className="max-h-80 overflow-y-auto">
+            {items.map((item, index) => (
               <div key={index} className="px-6 mb-2">
                 <DropdownMenuItem
                   asChild
                   className="px-3 py-2 flex justify-between items-center bg-hover group/link w-full rounded-md cursor-pointer focus:bg-hover focus:text-primary"
                 >
-                  <Link href={items.url} className="flex items-center w-full">
+                  <Link href={item.url} className="flex items-center w-full">
                     <div className="flex gap-3 w-full">
                       <h5 className="text-sm font-normal group-hover/link:text-primary">
-                        {items.title}
+                        {item.title}
                       </h5>
-                      {items.url === "/apps/invoice" && (
-                        <Badge variant={"outline"}>
-                          4
-                        </Badge>
+                      {item.url === "/apps/invoice" && (
+                        <Badge variant="outline">4</Badge>
                       )}
                     </div>
                   </Link>
                 </DropdownMenuItem>
               </div>
             ))}
-          </SimpleBar>
+          </div>
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
